89a021937a8e5a701c59d96717b517e068983fe4
Harry Chen <harry@harry.gg>
2016-03-15 23:13:25 +0800

    setup(deploy): Ready for Heroku

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
+require('dotenv').config({silent: true});
+
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var Pusher = require('pusher');
 var bodyParser = require('body-parser')
+
+app.set('port', (process.env.PORT || 3000));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,7 +14,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/reak_development');
+var MONGOLAB_URI = process.env.MONGOLAB_URI;
+mongoose.connect(MONGOLAB_URI);
+
 var Message = require('./models/message');
 
 var pusher = new Pusher({
@@ -50,6 +55,6 @@ app.post('/messages', function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log('Reak API listening on port 3000...')
+app.listen(app.get('port'), function() {
+  console.log('Reak API listening on port', app.get('port'));
 });
